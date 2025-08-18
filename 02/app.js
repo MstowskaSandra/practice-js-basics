f1a47f00dd73cdbf78c98055114c35a41c031070
Sandra <sandra.mstowskaa@gmail.com>
2025-08-18 14:40:41 +0200

    complete task 02

--- 02/app.js
+++ 02/app.js
@@ -1,6 +1,6 @@
 
 /* rozwiązanie z pętlą for */
-const x = prompt('Podaj liczbę!');
+const x = Number(prompt('Podaj liczbę od 1 do 9!'));
 
 if(x > 9) {
    alert("Za duża liczba, uczymy się do 9!");
@@ -9,7 +9,29 @@ if(x > 9) {
         const results = x * i;
         console.log(`${x} x ${i} = ${results}`);
     }
-}10
+}
+
 
 
 /* rozwiązanie z pętlą while  */
+
+const a = Number(prompt('Podaj podstawę potęgowania! (max liczba 10)'));
+const n = Number(prompt('Podaj wykładnij potęgowania! (max liczba 5)'));
+
+let result = 1;
+let sequence = '';
+
+
+if(a < 1 || a > 10 && n < 1 || n > 5) {
+    alert('Prosze podac prawidłowe liczby!');
+} else {
+    let i = 1;
+    while(i <= n) {
+        result *= a;
+        sequence += a;
+        if( i < n) sequence += ' * ';
+        i++;
+    }
+    console.log(`${sequence} = ${result}`);
+}
+
