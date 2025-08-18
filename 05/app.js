e707d5959fd5ae6f69043ac433125a3f9c08942a
Sandra <sandra.mstowskaa@gmail.com>
2025-08-18 19:05:30 +0200

    complete task 05/ complete all tasks

---
+++ 05/app.js
@@ -0,0 +1,40 @@
+function Student(initFirstName, initLastName, initGrades) {
+    this.firstName = initFirstName;
+    this.lastName = initLastName;
+    this.grades = {};
+}
+
+Student.prototype.addGrade = function(subject, grade) {
+    if (typeof this.grades[subject] === 'undefined') {
+        this.grades[subject] = [];
+    }
+    this.grades[subject].push(grade);
+};
+
+const student = new Student('Sandra', 'Mstowska');
+student.addGrade('maths', 4);
+student.addGrade('maths', 5);
+student.addGrade('english', 3);
+student.addGrade('english', 5);
+
+
+Student.prototype.getAverageGrade = function(subject) {
+    if (subject) {
+        const grades = this.grades[subject];
+        if (!grades || grades.length === 0) return 0;
+        const sum = grades.reduce((acc, val) => acc + val, 0);
+        return sum /grades.length;
+    } else {
+        const allGrades = Object.values(this.grades).flat();
+        if (allGrades.length === 0) return 0;
+        const sum = allGrades.reduce((acc, val) => acc + val, 0);
+        return sum /allGrades.length;
+    }
+};
+
+student.addGrade('polish', 1);
+
+console.log(student);
+console.log("Średnia wszytskich ocen:", student.getAverageGrade());
+console.log("Średnia ocen z matmy:", student.getAverageGrade('maths'));
+console.log("Średnia ocen z polski język:", student.getAverageGrade('polish'));
