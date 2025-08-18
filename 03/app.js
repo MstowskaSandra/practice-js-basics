bb59c2602d020721f3008c782efe010bf55a1298
Sandra <sandra.mstowskaa@gmail.com>
2025-08-18 17:18:58 +0200

    complete task 03

--- 03/app.js
+++ 03/app.js
@@ -7,10 +7,44 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum(a, b, c) {
+    const numbers = [a, b, c];
+    numbers.sort(function(a, b){ return b-a;});
+    console.log("sort numbers:", numbers);
+    const sum = numbers[0] + numbers[1];
+    return sum;
+}
+const sum = getSum(a, b, c);
+console.log("Sum of two greater numbers:", sum);
+
+function isEven(num) {
+    if(typeof(num) === Number) {
+        return null;
+    } else {
+        if (num % 2 === 0 ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+const isSumEven = isEven(sum);
+console.log("Sum is even?", isSumEven);
 
 
-
-
+function showInfo(sum, isSumEven) {
+    switch(isSumEven) {
+        case null:
+            console.log(`Podany argument ${sum} nie jest liczbą`);
+            break;
+        case true:
+            console.log(`Podany argument ${sum} jest parzysty`);
+            break;
+        case false:
+            console.log(`Podany argument ${sum} jest nieparzysty`);   
+    }
+}
+const completeInfo = showInfo(sum, isSumEven);
 
 
 function randomNumber(min, max) {
