8e68e15ddfb267c29dbfb44450c47d61897aae79
Sandra <sandra.mstowskaa@gmail.com>
2025-08-18 13:59:09 +0200

    complete 1/2 of task 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,15 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+const x = prompt('Podaj liczbę!');
 
+if(x > 9) {
+   alert("Za duża liczba, uczymy się do 9!");
+} else {
+    for(let i =  1; i <= 9; i++) {
+        const results = x * i;
+        console.log(`${x} x ${i} = ${results}`);
+    }
+}10
 
 
 /* rozwiązanie z pętlą while  */
